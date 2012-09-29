47440a8eed561a0cd7019ea28a5f89bf99c4078d
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2012-09-29 12:57:26 +0530

    Fix for slider icon when in ipad screen mode.

--- web-app/js/libs/bootstrap-collapse.js
+++ web-app/js/libs/bootstrap-collapse.js
@@ -70,9 +70,9 @@
       this.$element[dimension](this.$element[0][scroll])
       //do only when event is fired from top navigation bar
       if(this.$element.hasClass('nav-collapse')) {
-		 $(".header").css("height","230px");
-		 $(".col").css("top","240px");
-		 $('#slide-toggle').css("margin-top","14.5em");
+		 $(".header").css("height","300px");
+		 $(".col").css("top","310px");
+		 $('#slide-toggle').css("margin-top","18em");
 	  }
     }
 
