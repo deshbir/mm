143f1bad7a1aae3923dc9838f1f7683af486b311
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2012-10-06 14:15:10 +0530

    Moving photos under images folder.

--- grails-app/domain/com/cengage/mm/Photo.groovy
+++ grails-app/domain/com/cengage/mm/Photo.groovy
@@ -23,7 +23,7 @@ class Photo {
 
 		//returns list of models
 		def static JSON initialData() {
-			def o = JSON.parse ("[{'dir':'magazinedata/photos/img','fullfilename':'pic1.jpg','thumbfilename':'pic1.jpg','alt':'pic1' },{'dir':'magazinedata/photos/img','fullfilename':'pic2.jpg','thumbfilename':'pic2.jpg','alt':'pic2'},{'dir':'magazinedata/photos/img','fullfilename':'pic3.jpg','thumbfilename':'pic3.jpg','alt':'pic3'}]")
+			def o = JSON.parse ("[{'dir':'images/magazine/photos','fullfilename':'pic1.jpg','thumbfilename':'pic1.jpg','alt':'pic1' },{'dir':'images/magazine/photos','fullfilename':'pic2.jpg','thumbfilename':'pic2.jpg','alt':'pic2'},{'dir':'images/magazine/photos','fullfilename':'pic3.jpg','thumbfilename':'pic3.jpg','alt':'pic3'}]")
 			return o as JSON
 		}
 		
